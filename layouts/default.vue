6810a7e885547067fffe618b52f761e9484d08c4
Narin <narin.tana@hotmail.com>
2022-03-29 08:06:49 +0100

    Add logo hyperlink

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-full min-h-screen bg-[#19191d] p-3">
     <div class="max-w-screen-lg mx-auto h-full">
-      <img src="~/assets/logo.png" class="w-[15%]">
+      <NuxtLink to="/">
+        <img src="~/assets/logo.png" class="w-[15%]">
+      </NuxtLink>
       <Nuxt />
     </div>
   </div>

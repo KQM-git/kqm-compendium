3d82369bfe852a6e8cfe7095fdee4bdc41217fe4
Narin <narin.tana@hotmail.com>
2022-04-06 15:05:44 +0100

    Remove redundant imports

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 p-5">
-    <composition-preview
+  <div class="grid grid-cols-1 lg:grid-cols-2 gap-5">
+    <CompositionPreview
       v-for="composition of compositions.filter(x => x.extension == '.md')"
       :key="composition.slug"
       :composition="composition"
@@ -11,11 +11,9 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import CompositionPreview from '~/components/CompositionPreview.vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  components: { CompositionPreview },
   async asyncData ({ $content }) {
     const compositions = await $content('comps', { deep: true }).where({ extension: '.md' }).fetch()
     const characters = await $content('comps', { deep: true }).where({ extension: '.yaml' }).sortBy('position').fetch()

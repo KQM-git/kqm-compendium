e770b8b4d63c62010a00abaca9fdb12fd7c57920
Narin <narin.tana@hotmail.com>
2022-08-19 11:16:11 +0100

    Fix banners

--- components/PageBanner.vue
+++ components/PageBanner.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="relative w-full px-3">
+    <div class="relative px-3">
         <div class="absolute bottom-0 z-10 text-white font-bold text-2xl sm:text-5xl m-3 sm:m-5">
             {{ name }}
         </div>
-        <img :src="require(`~/assets/banners/${imageName}`)" class="brightness-50 rounded-3xl">
+        <img :src="require(`~/assets/banners/${imageName}`)" class="w-full h-28 sm:h-40 lg:h-60 object-cover brightness-50 rounded-3xl">
     </div>
 </template>
 
